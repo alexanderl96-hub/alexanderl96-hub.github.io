a8c53aa0e240aa0b5f90643fbea86127feedae52
Alexander <alexanderl96@yahoo.com>
2021-03-30 16:08:49 -0400

    done

--- skills.js
+++ skills.js
@@ -2,15 +2,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
   let skills = document.getElementById('skills2');
   const paraskills = document.getElementById('paraskills')
   const forms = document.createElement('div')
+  forms.innerHTML = `<img src= "images/react js.png"><img src= "images/css.png">
+  <img src= "images/html.png"><img src= "images/node .png"><img src= "images/postgresql.jpeg"> 
+  <img src= "images/javascript.png"> <img src= "images/auditio.png"><img src= "images/photoshow.png">
+  <img src="images/gif/premierpro.jpeg"> `
+  paraskills.appendChild(forms)
   forms.className = 'cardAll'
   skills.addEventListener('change', ()=>{
-    if(skills.value === 'all'){
-      forms.innerHTML = `<img src= "images/react js.png"><img src= "images/css.png">
-      <img src= "images/html.png"><img src= "images/node .png"><img src= "images/postgresql.jpeg"> 
-      <img src= "images/javascript.png"> <img src= "images/auditio.png"><img src= "images/photoshow.png">
-      <img src="images/gif/premierpro.jpeg"> `
-      paraskills.appendChild(forms)
-    }else if(skills.value === 'front'){
+    if(skills.value === 'front'){
       forms.innerHTML = `<img src= "images/react js.png"><img src= "images/css.png">
       <img src= "images/html.png">`
       paraskills.appendChild(forms)
@@ -23,7 +22,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
       <img src="images/gif/premierpro.jpeg">`
       paraskills.appendChild(forms)
     }else{
-      forms.innerHTML= ''
+      forms.innerHTML= `<img src= "images/react js.png"><img src= "images/css.png">
+      <img src= "images/html.png"><img src= "images/node .png"><img src= "images/postgresql.jpeg"> 
+      <img src= "images/javascript.png"> <img src= "images/auditio.png"><img src= "images/photoshow.png">
+      <img src="images/gif/premierpro.jpeg"> `
+      paraskills.appendChild(forms)
     }
     });
     
